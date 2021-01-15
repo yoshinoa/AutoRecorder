6c11380545a9409b7a162d3b1e3b5e1e9bd80e17
yoshinoa <alexander.yoshino@mail.utoronto.ca>
2021-01-16 00:26:00 +0900

    fixed colors

--- functions.py
+++ functions.py
@@ -1,17 +1,17 @@
 
 def get_information() -> (str, int, str):
     link = input("What's your zoom link?\n")
-    print(f"\033[92mZoom link is\033[0m {link}")
+    print(f"Zoom link is {link}")
     while True:
         try:
             meeting_hour = int(input("What time does your meeting start? (0-23)\n"))
         except ValueError:
             meeting_hour = int(input("Incorrect value, input again.\n"))
         break
-    print(f"\033[92mStart time is {meeting_hour}\033[0m")
+    print(f"Start time is {meeting_hour}")
     passcode = input("If there is a passcode enter it now.\n")
     if passcode != "":
-        print(f"\033[92mPasscode is {passcode}.\033[0m")
+        print(f"Passcode is {passcode}.")
     else:
-        print(f"\033[92mNo passcode recorded.\033[0m")
+        print(f"No passcode recorded.")
     return link, meeting_hour, passcode
